ef2ec2b960b6e7ec84b2044ff5743a68abdbcbe3
VinylStage <mcpe9869@gmail.com>
2023-04-20 17:56:56 +0900

    init project

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("hello");
+
+window.onload = async function loadArticles() {
+  const response = await fetch("http://127.0.0.1:8000/articles/", {method: "GET"});
+  response_json = await response.json();
+
+  console.log(response_json);
+
+  const articles = document.getElementById("articles");
+
+  response_json.forEach(element => {
+    console.log(element.title);
+    const newArticle = document.createElement("div");
+    newArticle.innerText = element.title;
+    articles.appendChild(newArticle);
+  });
+};
